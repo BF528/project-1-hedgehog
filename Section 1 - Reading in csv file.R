5665760412cc487761c4a80dd9d384f529320d96
Bettenia Cole <bcole99@scc-wi2.scc.bu.edu>
2021-02-20 14:22:54 -0500

    Thanks to Saket this one got fixed! Thanks!

--- Section 1 - Reading in csv file.R
+++ Section 1 - Reading in csv file.R
@@ -5,4 +5,7 @@ raw_data_1<-read.csv("/projectnb/bf528/users/hedgehog/project_1/biologist role/#
 
 
 head(raw_data_1)
+raw_data<-read.csv("/project/bf528/project_1/data/differential_expression_results.csv")
 
+
+Synced_2GS<- AnnotationDbi::select(hgu133plus2.db, keys = raw_data_1$X, columns = ("SYMBOL"))
