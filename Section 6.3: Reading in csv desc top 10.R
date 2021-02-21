6581ae81c2c70dbe649ff37513a2ad05114d84b9
Bettenia Cole <bcole99@scc-wi2.scc.bu.edu>
2021-02-21 18:35:44 -0500

    updated

--- Section 6.3: Reading in csv desc top 10.R
+++ Section 6.3: Reading in csv desc top 10.R
@@ -7,6 +7,7 @@ str(ttest_results)
 
 ttest_results_B<-ttest_results %>% top_n(10, desc(p_adju))%>%arrange(p_adju)
 str(ttest_results_B)
+ttest_Bs<-format(ttest_results_B, digits = 4, scientific = 2)
 
 knitr::kable(
   ttest_Bs[],    caption = "Gene Sets by Lowest p-value (adjusted)."
