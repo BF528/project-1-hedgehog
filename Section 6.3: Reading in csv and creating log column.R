6a7cf5fff0ffdd4fe770e2ab6a695f9232b8e93c
Bettenia Cole <bcole99@scc-wi2.scc.bu.edu>
2021-02-21 13:43:12 -0500

    added arranging by adjusted p-value

--- Section 6.3: Reading in csv and creating log column.R
+++ Section 6.3: Reading in csv and creating log column.R
@@ -3,9 +3,6 @@
 ttest_results<-read.csv("/projectnb/bf528/users/hedgehog/project_1/noise_filtering/clustered_ttests.csv")
 head(ttest_results)
 ttest_results$lfch<-logratio2foldchange(ttest_results$p_adju, base=2)
-
-
-
-
+ttest_r_pvalue<-arrange(ttest_results, desc(ttest_results$p_adju))
 
 
