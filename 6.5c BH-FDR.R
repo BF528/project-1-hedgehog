4e078c5792573b7224f8b492b3ff65efbcbd2d77
Bettenia Cole <bcole99@scc-wi4.scc.bu.edu>
2021-02-21 22:28:41 -0500

    updated to calculate  BH

--- 6.5c BH-FDR.R
+++ 6.5c BH-FDR.R
@@ -4,6 +4,7 @@ install.packages(sgof)
 library(sgof)
 ttest_results<-read.csv("/projectnb/bf528/users/hedgehog/project_1/noise_filtering/clustered_ttests.csv")
 head(ttest_results)
+p.adjust(ttest_results$p_valu)
 
 
 
