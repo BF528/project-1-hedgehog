03b04b32adf7d18a532673d6056245239f2c11eb
Bettenia Cole <bcole99@scc-ge2.scc.bu.edu>
2021-02-23 00:31:47 -0500

    updated code

--- 6.5c BH-FDR.R
+++ 6.5c BH-FDR.R
@@ -1,11 +1,26 @@
-
+library(dplyr)
+library(rlang)
+library(purrr)
+library(stringr)
+library(tidyverse)
+library(tidyselect)
+library(base)
 
 install.packages(sgof)
 library(sgof)
 ttest_wBH<-read.csv("/projectnb/bf528/users/hedgehog/project_1/noise_filtering/clustered_ttests.csv")
-head(ttest_results)
-
-
-
-ttest_wBH$FDR<-p.adjust(ttest_results$p_valu, method = "hochberg")
 head(ttest_wBH)
+
+ttest_wBHm<-format(ttest_wBH, digits = 3, nsmall= 20, exp(30), scientific = TRUE)
+ttest_wBHm$FDR<-p.adjust(ttest_wBHm$p_valu, method = "BH")
+FDRn10<-head(ttest_wBHm,10)%>%arrange(FDR)
+FDRn10
+FDRn3<-head(ttest_wBHm,3)%>%arrange(FDR)
+FDRn3
+summary(FDRn3)
+
+
+knitr::kable(
+  FDRn3[],    
+  caption = "Top 3 Gene Sets (with BH-FDR) Analysis."
+)
